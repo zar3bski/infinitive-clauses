6f06683c010a9c628ee2a86e0643e615aaddb82f
zar3bski <zarebskidavid@gmail.com>
2021-03-18 20:02:18 +0100

    wip : workign around with spacy

--- infinitive_clauses/nlp.py
+++ infinitive_clauses/nlp.py
@@ -1,4 +1,19 @@
 import spacy
+import os
+import pickle
 
 nlp = spacy.load("en_core_web_trf")
 
+if os.path.exists("{}/infinitive_clauses/__pycache__/64843-0.pickle".format(os.getcwd())):
+    parsed = pickle.load(open("{}/infinitive_clauses/__pycache__/64843-0.pickle".format(os.getcwd()),"rb"))
+else:
+    with open("{}/tests/texts/64843-0.txt".format(os.getcwd())) as text: 
+        parsed = nlp(text.read()[100:78000])
+        pickle.dump(parsed, open("{}/infinitive_clauses/__pycache__/64843-0.pickle".format(os.getcwd()),"wb"))
+
+
+print(dir(parsed))
+
+#print("Noun phrases:", [chunk.text for chunk in parsed if chunk.pos_ == "VERB"])
+
+print("First sentences: ", [sentence for sentence in parsed.sents][47:67])
--- tests/test_infinitive_clauses.py
+++ tests/test_infinitive_clauses.py
@@ -1,5 +1,5 @@
 from infinitive_clauses import __version__
-
+from infinitive_clauses import nlp
 
 def test_version():
     assert __version__ == '0.1.0'
